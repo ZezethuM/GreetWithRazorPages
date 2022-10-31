dbf0e156313cc566f129c3fb1d65906eee89fd53
ZezethuM <zezethu15@gmail.com>
2022-10-31 16:37:43 +0200

    mongodb

--- razor-pages-greet/Program.cs
+++ razor-pages-greet/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<IGreet, GreetWithBD>();
+builder.Services.AddSingleton<IGreet, MongoGreet>();
 
 var app = builder.Build();
 
